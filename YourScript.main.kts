7c172aa17d6d4969f9586ba9b672469a95d1e876
Yasutaka Kawamoto <kwmt27@gmail.com>
2024-01-06 19:25:58 +0900

    update

--- YourScript.main.kts
+++ YourScript.main.kts
@@ -1,5 +1,22 @@
-#!/usr/bin/env kotlin
+import java.io.File
+val file = File("versions.toml")
 
-fun main() {
-    prlntln("hello world")
+var result = ""
+file.readLines().forEach {
+    println(it)
+    val list = it.split("=")
+    println(list)
+    if (list.size >= 2) {
+        if(list[0] == "pluginVersion" ) {
+            val (major, minor, patch) = list[1].split(".")
+            val newPatch = patch.toInt()  + 1 
+            result = listOf(major, minor, newPatch).joinToString(".")
+            println(result)
+            if (result.isNotEmpty()) {
+                return@forEach
+            }
+        }
+    }
 }
+println(result)
+
